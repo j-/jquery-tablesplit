d62e8bf6810b771a3ba35dd805f239f5bf72b88f
j- <jh@live.com.au>
2015-01-09 15:16:42 +1100

    Added jQuery table split plugin

---
+++ jquery.tablesplit.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+$.fn.tableSplit = function (index, options) {
+	var $table = this.first();
+	var $left = $table.tableSlice(0, index, options);
+	var $right = $table.tableSlice(index, options);
+	var $result = $($left).add($right);
+	return $result;
+};
+
+})(jQuery);
